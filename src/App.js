6b56f57adfae370b431eaf61045864a59b6fba41
lotus-maker <96130396+lotus-maker@users.noreply.github.com>
2025-02-17 18:03:24 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import ReactGA from "react-ga";
-import $ from "jquery";
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
@@ -8,6 +7,7 @@ import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
+import Star from "./Components/Star";
 
 class App extends Component {
   constructor(props) {
@@ -21,19 +21,15 @@ class App extends Component {
     ReactGA.pageview(window.location.pathname);
   }
 
-  getResumeData() {
-    $.ajax({
-      url: "./resumeData.json",
-      dataType: "json",
-      cache: false,
-      success: function(data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
+  async getResumeData() {
+    try {
+      const response = await fetch("./resumeData.json");
+      const data = await response.json();
+      this.setState({ resumeData: data });
+    } catch (err) {
+      console.error(err);
+      alert(err);
+    }
   }
 
   componentDidMount() {
@@ -49,6 +45,7 @@ class App extends Component {
         <Portfolio data={this.state.resumeData.portfolio} />
         <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
+        <Star data={this.state.resumeData.main} />
       </div>
     );
   }
